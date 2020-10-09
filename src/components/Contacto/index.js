550bc757a57260f3eff7c598358a02a9402d24ec
Pablo Villamizar <pablomvillamizar@gmail.com>
2020-10-09 08:23:38 -0400

    emailjs

--- src/components/Contacto/index.js
+++ src/components/Contacto/index.js
@@ -1,4 +1,6 @@
 import React,{useState} from 'react';
+import emailjs from 'emailjs-com'
+
 import {
   Section,
   Title,
@@ -10,8 +12,6 @@ import {
   Button,
 } from './styles';
 
-import { ENTRYPOINT } from '../../config/entrypoint'
-
 const Contacto = () => {
 
   const [data,setData] = useState({
@@ -19,19 +19,19 @@ const Contacto = () => {
     subject: '',
     message: ''
   });
-
+  
   const handleSubmit = form => {
     form.preventDefault()
-    fetch(`${ENTRYPOINT}/email`,{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(response => response.json())
+
+    let emailDetails = {
+      from_name: 'codelabst.com',
+      from: data.from,
+      subject: data.subject,
+      message: data.message,
+    }
+
+    emailjs.send("service_2z3pzoy","template_e0lugdi",emailDetails,'user_2c1uosaLCjmm3BRDrhdka')
       .then(response => {
-        console.log(response)
         swal('CodeLab','Mensaje Enviado!','success');
         setData({
           from: '',
@@ -39,8 +39,10 @@ const Contacto = () => {
           message: ''
         })
       })
-      .catch(error => swal('CodeLab','Lo siento hubo un error','error'))
-      // console.error(error)
+      .catch(error => {
+        console.error(error)
+        swal('CodeLab','Lo siento hubo un error','error')
+      })
   }
 
   const handleChange = input => {
@@ -60,9 +62,9 @@ const Contacto = () => {
         </Parr>
       </ContainerInfo>
       <Formulario onSubmit={handleSubmit} >
-        <Input type='text' value={data.subject} name='subject' onChange={handleChange} placeholder='Asunto' />
-        <Input type='text' value={data.from} name='from' onChange={handleChange} placeholder='Correo electrónico' />
-        <TextBox type='text' value={data.message} name='message' onChange={handleChange} placeholder='Describe tu proyecto' />
+        <Input required type='text' value={data.subject} name='subject' onChange={handleChange} placeholder='Asunto' />
+        <Input required type='text' value={data.from} name='from' onChange={handleChange} placeholder='Correo electrónico' />
+        <TextBox required type='text' value={data.message} name='message' onChange={handleChange} placeholder='Describe tu proyecto' />
         <Button>Enviar</Button>
       </Formulario>
     </Section>
