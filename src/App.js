c5d3729f66ab350bf7440cd4b9bdd9d0e347e4b5
Pablo Villamizar <pablomvillamizar@gmail.com>
2020-10-12 22:24:37 -0400

    production

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route,BrowserRouter,Switch } from 'react-router-dom';
+import { Route,HashRouter ,Switch } from 'react-router-dom';
 import { GlobalStyles } from './styles/GlobalStyles';
 import  Home  from './pages/Home.js';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter >
       <GlobalStyles />
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter >
   );
 };
